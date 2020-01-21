ef8102339d2c5985a145b269122af40776391377
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-01-21 10:28:42 -0300

    ♻️ Add logic to correct unsubscribe

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -25,7 +25,9 @@ export class LoginComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy() {
-    this.subscription.unsubscribe();
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 
   onSubmit(form: NgForm) {
--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -21,7 +21,9 @@ export class RegistroComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy() {
-    this.subscription.unsubscribe();
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 
   onSubmit(form: NgForm) {
